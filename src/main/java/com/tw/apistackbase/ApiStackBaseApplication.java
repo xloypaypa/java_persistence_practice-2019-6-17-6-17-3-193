d4cba8a36a493b781ef196ff27274db9da5db4eb
suxin <xloypaypa@gmail.com>
2019-07-16 13:06:24 +0800

    create db

--- src/main/java/com/tw/apistackbase/ApiStackBaseApplication.java
+++ src/main/java/com/tw/apistackbase/ApiStackBaseApplication.java
@@ -3,10 +3,40 @@ package com.tw.apistackbase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @SpringBootApplication
 public class ApiStackBaseApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.h2.Driver");
+            conn = DriverManager.getConnection("jdbc:h2:./h2/org", "sa", "");
+
+            stmt = conn.createStatement();
+
+            int result = stmt.executeUpdate("CREATE TABLE COMPANY (" +
+                    "id BIGINT NOT NULL, " +
+                    "name VARCHAR(255) NOT NULL," +
+                    "PRIMARY KEY (id)" +
+                    ");");
+
+            System.out.println("result:" + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
 		SpringApplication.run(ApiStackBaseApplication.class, args);
-	}
+    }
 }
